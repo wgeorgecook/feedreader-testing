25f47b365013bbedbbe39e3fcfba18eb81b065da
Will Cook <willcook@admins-MacBook-Air.local>
2018-07-18 11:19:46 -0700

    RSS tests complete skeleton for other suites

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,8 +31,11 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-        it('should have associated url', function() {
-            // test happens here
+        it('should have associated urls', function() {
+            for (item of allFeeds) {
+                expect(item.url).toBeDefined();
+                expect(item.url).not.toBe('');
+            };
         });
 
         /* TODO: Write a test that loops through each feed
@@ -40,41 +43,61 @@ $(function() {
          * and that the name is not empty.
          */
 
-        it('should have associated name', function() {
-            // test happens here
+        it('should have associated names', function() {
+            for (item of allFeeds) {
+                expect(item.name).toBeDefined();
+                expect(item.name).not.toBe('');
+            };
         });
     });
 
 
     /* TODO: Write a new test suite named "The menu" */
-    descripe('The menu', function() {
+    describe('The menu', function() {
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
+        it('should be hidden by default', function() {
+            // look for the display attribute 
+        });
 
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
+        it('should display when menu is clicked', function() {
+            // call the menuIcon.onclick function from app.js
+            // and check for display attribute
+        });
+        
+    });
 
     /* TODO: Write a new test suite named "Initial Entries" */
-
+    describe('Initial entries', function() {
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+        it('should have entries', function() {
+            // do magic
+        });
+    });
 
     /* TODO: Write a new test suite named "New Feed Selection" */
-
+    describe('New Feed Selection', function() {
+        
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+        it('should display correct content', function() {
+            // magic
+        });
     });
 
 }());
